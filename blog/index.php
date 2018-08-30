2634193847bb4ae6599f675f5abc6a58c9bd6426
Friday Godswill <faradayyg@gmail.com>
2018-08-30 01:20:52 +0100

    added google analytics

--- blog/index.php
+++ blog/index.php
@@ -44,6 +44,15 @@ catch (SQLiteException $e) {
 ?>
 <html lang="en">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-65826189-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-65826189-2');
+    </script>
     <meta charset="utf-8" />
     <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
